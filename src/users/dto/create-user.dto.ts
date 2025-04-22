a80290530bb33f9d7dbf6365e6b22007b035e7a1
HernanBareiro <hernanbareiro@gmail.com>
2025-04-22 18:31:31 -0300

    First commit. Add CRUD user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+    email: string;
+    password: string;
+    firstNames: string;
+    lastName: string; 
+    phoneNumber: string;
+    dateOfBirth: Date;
+    identityNumber: string;
+    identytyType: string;
+}
