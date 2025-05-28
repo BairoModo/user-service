1f05e362d4951e088e70514d7dd25fe8192a554a
HernanBareiro <hernanbareiro@gmail.com>
2025-05-27 22:16:52 -0300

    add request validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,36 @@
+import { IsEmail, IsString, IsNotEmpty, IsOptional, IsDate, IsPhoneNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
 export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
+
+    @IsString()
+    @IsNotEmpty()
     firstNames: string;
+
+    @IsString()
+    @IsNotEmpty()
     lastName: string; 
+
+    @IsPhoneNumber()
+    @IsNotEmpty()
     phoneNumber: string;
+
+    @IsDate()
+    @IsNotEmpty()
+    @Transform(({ value }) => new Date(value))
     dateOfBirth: Date;
-    identityNumber: string;
-    identytyType: string;
+
+    @IsOptional()
+    @IsString()
+    identityNumber?: string;
+
+    @IsOptional()
+    @IsString()
+    identytyType?: string;
 }
